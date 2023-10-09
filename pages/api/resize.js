b3770c3f71dbccad54b325a2916eefae74cacdc0
Mark Sergienko <ffab00@gmail.com>
2023-10-09 14:19:15 +0100

    resizer supports relative paths with multiple hosts

--- pages/api/resize.js
+++ pages/api/resize.js
@@ -1,13 +1,36 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import axios from "axios";
 import sharp from "sharp";
+import url from "url";
 
 export default async function handler(req, res) {
-  const { url, width, quality } = req.query;
+  const { url: imageUrl, width, quality } = req.query;
+  const hosts = process.env.IMAGE_HOSTS.split(",");
 
   try {
-    const response = await axios.get(url, { responseType: "arraybuffer" });
-    const buffer = Buffer.from(response.data, "binary");
+    let buffer;
+    if (imageUrl.startsWith("http")) {
+      const response = await axios.get(imageUrl, {
+        responseType: "arraybuffer",
+      });
+      buffer = Buffer.from(response.data, "binary");
+    } else {
+      for (const host of hosts) {
+        const fullUrl = url.resolve(host, imageUrl);
+        const response = await axios
+          .get(fullUrl, { responseType: "arraybuffer" })
+          .catch(() => null);
+        if (response) {
+          buffer = Buffer.from(response.data, "binary");
+          break;
+        }
+      }
+    }
+
+    if (!buffer) {
+      res.status(404).json({ error: "Image not found" });
+      return;
+    }
 
     const resizedBuffer = await sharp(buffer)
       .resize(parseInt(width))
