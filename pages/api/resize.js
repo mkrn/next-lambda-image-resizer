926010c350fc47f442fa979aded62f288477e74d
Mark Sergienko <ffab00@gmail.com>
2023-10-09 13:41:20 +0100

    resizer code

--- pages/api/resize.js
+++ pages/api/resize.js
@@ -1,5 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios";
+import sharp from "sharp";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const { url, width, quality } = req.query;
+
+  try {
+    const response = await axios.get(url, { responseType: "arraybuffer" });
+    const buffer = Buffer.from(response.data, "binary");
+
+    const resizedBuffer = await sharp(buffer)
+      .resize(parseInt(width))
+      .jpeg({ quality: parseInt(quality) })
+      .toBuffer();
+
+    res.setHeader("Content-Type", "image/jpeg");
+    res.status(200).end(resizedBuffer);
+  } catch (error) {
+    res.status(500).json({ error: "Error resizing the image" });
+  }
 }
